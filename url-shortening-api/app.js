215912196123ed9346df0f81fff2a5139c3ab263
Alin1001 <alin.cr23@gmail.com>
2021-04-05 13:50:34 +0300

    Added Javascript

---
+++ url-shortening-api/app.js
@@ -0,0 +1,78 @@
+const input = document.querySelector("input");
+const submitBtn = document.querySelector("submit");
+const form = document.querySelector("form");
+const loadingText = document.querySelector(".loading");
+
+let isLoading = true;
+
+const getInputData = (e) => {
+  e.preventDefault();
+  const value = input.value;
+  console.log(value);
+
+  if (!value) {
+    input.classList.add("warning");
+
+    return;
+  }
+  input.classList.remove("warning");
+  getData(value);
+};
+
+// Axios getdata from Server
+
+const getData = (param) => {
+  isLoading = true;
+
+  loadingText.style.display = "block";
+  let url = `
+  https://api.shrtco.de/v2/shorten?url=${param}`;
+
+  axios
+    .get(url)
+    .then((resp) => {
+      isLoading = false;
+      loadingText.style.display = "none";
+      console.log(resp.data);
+      let param2 = resp.data.result.full_short_link;
+      resultBox(param, param2);
+    })
+    .catch((err) => {
+      console.log(err);
+      isLoading = false;
+    });
+};
+form.addEventListener("submit", getInputData);
+
+// Function that create a div every time it runs;
+const resultBox = (firstValue, secondValue) => {
+  const mainDiv = document.querySelector(".results");
+
+  const linkItem = document.createElement("div");
+  linkItem.classList.add("link");
+
+  linkItem.innerHTML = `
+    <span class="original-link">${firstValue}</span>
+    <span class="new-link">${secondValue}</span>
+    <button class="submit copy">Copy</button>
+    `;
+
+  mainDiv.append(linkItem);
+};
+
+const handleCopy = (e) => {
+  if (e.target.classList.contains("copy")) {
+    const btn = e.target;
+    const tempInput = document.createElement("input");
+    const newLink = btn.previousElementSibling.innerText;
+    tempInput.setAttribute("value", newLink);
+    document.body.append(tempInput);
+    tempInput.select();
+
+    btn.innerText = "Copied";
+    btn.classList.add("copied");
+    document.execCommand("copy");
+    tempInput.parentNode.removeChild(tempInput);
+  }
+};
+document.addEventListener("click", handleCopy);
